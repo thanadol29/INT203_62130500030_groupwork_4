5e256233d9c079e412dc5b145f9fc983c165a249
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-02-19 20:51:53 +0700

    Add/Edit
    Add number of city
    Add grid
    Add clickable heart
    Add preview image
    Add cancel text box
    Edit page layout
    Edit magnify glass that can pop in

--- 62130500030_groupwork_4/main.js
+++ 62130500030_groupwork_4/main.js
@@ -1,8 +1,9 @@
 class picture {
-  constructor(name, url,like) {
+  constructor(name, url, like, bigpic) {
     this.name = name;
     this.url = url;
     this.like = like;
+    this.bigpic = bigpic;
   }
 }
 
@@ -11,12 +12,12 @@ const app = {
   data() {
     return {
       search: '',
-      pics: [{ name: "Moscow", url: "https://i.imgur.com/B0zvdp6.png", like: false },
-      { name: "Berlin", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Berlin_reichstag_west_panorama_2.jpg/1920px-Berlin_reichstag_west_panorama_2.jpg", like: false },
-      { name: "Vienna", url: "https://i.imgur.com/5tRu76I.jpg", like: false }
+      pics: [{ name: "Moscow", url: "https://www.planete-energies.com/sites/default/files/styles/media_full_width_940px/public/thumbnails/image/moscow.jpg?itok=J_vRU4rA", like: false , bigpic: false},
+      { name: "Berlin", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Berlin_reichstag_west_panorama_2.jpg/1920px-Berlin_reichstag_west_panorama_2.jpg", like: false  , bigpic: false},
+      { name: "Vienna", url: "https://www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/01/Karlskirche_Wien_abends-1024x576.jpg", like: false  , bigpic: false}
       ],
-      showsearch: false
-
+      showsearch: false,
+      showmag: true
     }
 
   },
@@ -25,7 +26,20 @@ const app = {
       return this.pics.filter(picture => {
         return picture.name.toLowerCase().includes(this.search.toLowerCase())
       })
+    },
+    mag() {
+      this.showmag = !this.showmag;
+    },
+    test(){
+       return this.pics.length;
     }
+    ,
+  methods: {
+      clear() {
+        this.search = '';
+      }
+    }
+
   }
 
 
